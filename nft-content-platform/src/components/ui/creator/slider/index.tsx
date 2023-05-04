0faaf2b75f20f1871219ab751baf0cb56a72a471
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-05-04 06:45:12 +0530

    text-update

--- nft-content-platform/src/components/ui/creator/slider/index.tsx
+++ nft-content-platform/src/components/ui/creator/slider/index.tsx
@@ -60,7 +60,7 @@ export default function CreatorSlider() {
             Explore Creators
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Discover top 10 creators in the space
+            Top 10 creators in the leaderboard
           </p>
         </div>
         <div className="mt-10">
